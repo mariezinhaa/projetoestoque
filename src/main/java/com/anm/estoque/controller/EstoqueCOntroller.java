bdcec11c37944f8d47b545ce924decc7888f574e
Maria <hosangela.acop@gmail.com>
2025-11-17 11:23:49 -0300

    ProjetoEstoque

--- src/main/java/com/anm/estoque/controller/EstoqueCOntroller.java
+++ src/main/java/com/anm/estoque/controller/EstoqueCOntroller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class EstoqueCOntroller {
+    
     @GetMapping("/")
     public void testeApi() {
        System.out.println("A API está funcionando!");
